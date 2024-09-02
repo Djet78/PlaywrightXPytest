3f4209c9279b3e75d6807c5294662ecd82124a6c
Viacheslav <vacheslavuslistyj@gmail.com>
2024-09-02 16:22:28 +0300

    Added demo workflow

--- playwright_pytest/ui_tests/test_examples.py
+++ playwright_pytest/ui_tests/test_examples.py
@@ -1,7 +1,10 @@
+import pytest
+
 from playwright.sync_api import Page, expect
 from playwright_pytest.playwright_pytest.ui_comp import PlaywrightSearch
 
 
+@pytest.mark.ui()
 def test_simple_search(page: Page):
     page.goto('https://playwright.dev/python/docs/input')
 
@@ -11,6 +14,7 @@ def test_simple_search(page: Page):
     expect(s_bar.search_field).to_have_value('Playwright')
 
 
+@pytest.mark.ui()
 def test_example(page: Page) -> None:
     page.goto('https://demo.playwright.dev/todomvc/')
     page.get_by_placeholder('What needs to be done?').click()
