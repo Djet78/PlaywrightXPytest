3aba7176fbb2a84d90c89fc039774833a61decd2
Viacheslav <vacheslavuslistyj@gmail.com>
2024-09-16 12:14:52 +0300

    updated lable inheritence

--- playwright_pytest/ui_tests/test_todo_app.py
+++ playwright_pytest/ui_tests/test_todo_app.py
@@ -6,28 +6,25 @@ from playwright_pytest.playwright_pytest.ui_comp import PlaywrightSearch
 
 @allure.suite('UI')
 @allure.feature('Docs search')
-@allure.title('Test Simple Search')
-@allure.description('Search for Playwright docs.')
-@allure.severity(allure.severity_level.NORMAL)
 @allure.label('owner', 'Viacheslav')
-def test_simple_search(page: Page):
-    page.goto('https://playwright.dev/python/docs/input')
+class TestSearch:
+    @allure.title('Test Simple Search')
+    @allure.description('Search for Playwright docs.')
+    @allure.severity(allure.severity_level.NORMAL)
+    def test_simple_search(self, page: Page):
+        page.goto('https://playwright.dev/python/docs/input')
 
-    s_bar = PlaywrightSearch(page)
-    s_bar.search_for('Playwright')
+        s_bar = PlaywrightSearch(page)
+        s_bar.search_for('Playwright')
 
-    expect(s_bar.search_field).to_have_value('Playwright')
+        expect(s_bar.search_field).to_have_value('Playwright')
 
-
-@allure.suite('UI')
-@allure.feature('Docs search')
-@allure.title('Test Add 1 TODO action')
-@allure.description('Simply add 1 TODO item.')
-@allure.severity(allure.severity_level.CRITICAL)
-@allure.label('owner', 'Viacheslav')
-def test_add_1_action(page: Page) -> None:
-    page.goto('https://demo.playwright.dev/todomvc/')
-    page.get_by_placeholder('What needs to be done?').click()
-    page.get_by_placeholder('What needs to be done?').fill('action 1')
-    page.get_by_placeholder('What needs to be done?').press('Enter')
-    expect(page.get_by_test_id('todo-title')).to_contain_text('action 1')
+    @allure.title('Test Add 1 TODO action')
+    @allure.description('Simply add 1 TODO item.')
+    @allure.severity(allure.severity_level.CRITICAL)
+    def test_add_1_action(self, page: Page) -> None:
+        page.goto('https://demo.playwright.dev/todomvc/')
+        page.get_by_placeholder('What needs to be done?').click()
+        page.get_by_placeholder('What needs to be done?').fill('action 1')
+        page.get_by_placeholder('What needs to be done?').press('Enter')
+        expect(page.get_by_test_id('todo-title')).to_contain_text('action 1')
